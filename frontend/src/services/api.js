762cb3c0f8b311d1b62f83111fc314ce9af3cb41
CeliaGaudin <lenoircelia@hotmail.fr>
2026-07-23 11:35:20 +0200

    Fix React API proxy with Nginx and PHP-FPM

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,28 +1,34 @@
-const API_URL = 'http://localhost:8000'
+const API_URL = "/api";
 
 export async function getUsers() {
-  const response = await fetch(`${API_URL}/api/users`)
+    const response = await fetch(`${API_URL}/users`);
 
-  if (!response.ok) {
-    throw new Error('Erreur lors de la récupération des utilisateurs')
-  }
+    if (!response.ok) {
+        throw new Error("Erreur lors du chargement des utilisateurs");
+    }
 
-  return response.json()
+    return response.json();
 }
 
 
-export async function createUser(user) {
-  const response = await fetch(`${API_URL}/api/users`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(user),
-  })
+export async function createUser(email) {
+    const response = await fetch(`${API_URL}/users`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            email: email,
+        }),
+    });
 
-  if (!response.ok) {
-    throw new Error('Erreur lors de la création de l’utilisateur')
-  }
+    if (!response.ok) {
+        throw new Error("Erreur lors de la création de l'utilisateur");
+    }
 
-  return response.json()
+    const text = await response.text();
+
+console.log("Réponse reçue :", text);
+
+return JSON.parse(text);
 }
